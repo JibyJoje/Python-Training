aea4d6df83e8362963071a7d611b730d2f436ba7
Jiby Joje <Jiby.Joje@Walmartlabs.com>
2021-01-17 11:29:59 +0530

    pushing Hello.py

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+#This is just a simple program that asks for your Name and Age and then prints the details on the screen
+
+print('Hi There!')
+print('What is your Name ?') #Enter your name here
+myName = input()
+print('Hello '+myName+' nice to meet you!')
+print('What is your age ?') # Enter your age here
+myAge = input()
+print('You will be '+str(int(myAge)+1) +' in the next year')
